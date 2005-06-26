ea996cd95e3b9f69b024ce5ed02ba9e6fc424c05
Shugo Maeda <shugo@ruby-lang.org>
2005-06-26 06:22:31 +0000

    added StaticMailbox#fetch.

--- tests/static-mailbox-test.rb
+++ tests/static-mailbox-test.rb
@@ -32,6 +32,7 @@ class XimapdStaticMailboxTest < Test::Unit::TestCase
     super
     @mail_store = Ximapd::MailStore.new(@config)
     @mail_store.mailbox_db.transaction do
+      @mail_store.mailbox_db["status"]["last_uid"] = 1000
       @mail_store.mailbox_db["status"]["last_mailbox_id"] += 1
       mailbox_id = @mail_store.mailbox_db["status"]["last_mailbox_id"]
       @mail_store.mailbox_db["mailboxes"]["static-mailbox"] = {
@@ -43,48 +44,87 @@ class XimapdStaticMailboxTest < Test::Unit::TestCase
       FileUtils.mkdir_p(File.expand_path("mailboxes/#{mailbox_id}",
                                          @config["data_dir"]))
     end
-  end
 
-  def test_status
-    mailbox = @mail_store.get_mailbox("static-mailbox")
-    status = mailbox.status
-    assert_equal(0, status.messages)
-    assert_equal(0, status.unseen)
-    assert_equal(0, status.recent)
-
-    mail1 = <<EOF.gsub(/\n/, "\r\n")
+    @mail1 = <<EOF.gsub(/\n/, "\r\n")
 From: shugo@ruby-lang.org
 Subject: hello
 Date: Wed, 30 Mar 2005 17:34:46 +0900
 
 Hello world
 EOF
-    uid2 = @mail_store.import_mail(mail1, "static-mailbox", "\\Seen")
-    @mail_store.mailbox_db.transaction do
-      mailbox_data = @mail_store.mailbox_db["mailboxes"]["static-mailbox"]
-      mailbox_data["last_peeked_uid"] = uid2
-    end
-
-    mailbox = @mail_store.get_mailbox("static-mailbox")
-    status = mailbox.status
-    assert_equal(1, status.messages)
-    assert_equal(0, status.unseen)
-    assert_equal(0, status.recent)
-
-    mail2 = <<EOF.gsub(/\n/, "\r\n")
+    @uid1 = @mail_store.import_mail(@mail1, "static-mailbox", "\\Seen")
+    @mail2 = <<EOF.gsub(/\n/, "\r\n")
 From: shugo@ruby-lang.org
 Subject: bye
-Date: Wed, 30 Mar 2005 19:21:09 +0900
+Date: Wed, 30 Mar 2005 19:18:02 +0900
 
 Goodbye world
 EOF
-    @mail_store.import_mail(mail1, "static-mailbox", "")
+    @uid2 = @mail_store.import_mail(@mail2, "static-mailbox", "\\Seen")
+    @mail3 = <<EOF.gsub(/\n/, "\r\n")
+From: shugo@ruby-lang.org
+Subject: test3
+Date: Wed, 30 Mar 2005 19:18:02 +0900
 
-    mailbox = @mail_store.get_mailbox("static-mailbox")
-    status = mailbox.status
-    assert_equal(2, status.messages)
-    assert_equal(1, status.unseen)
-    assert_equal(1, status.recent)
+This is test3
+EOF
+    @uid3 = @mail_store.import_mail(@mail3, "static-mailbox", "")
+    @mail_store.mailbox_db.transaction do
+      mailbox_data = @mail_store.mailbox_db["mailboxes"]["static-mailbox"]
+      mailbox_data["last_peeked_uid"] = @uid3
+    end
+    @mail4 = <<EOF.gsub(/\n/, "\r\n")
+From: shugo@ruby-lang.org
+Subject: test4
+Date: Wed, 30 Mar 2005 19:18:02 +0900
+
+This is test4
+EOF
+    @uid4 = @mail_store.import_mail(@mail4, "static-mailbox", "")
+    @mail5 = <<EOF.gsub(/\n/, "\r\n")
+From: shugo@ruby-lang.org
+Subject: test5
+Date: Wed, 30 Mar 2005 19:18:02 +0900
+
+This is test5
+EOF
+    @uid5 = @mail_store.import_mail(@mail5, "static-mailbox", "")
+
+    @mailbox = @mail_store.get_mailbox("static-mailbox")
+  end
+
+  def test_status
+    status = @mailbox.status
+    assert_equal(5, status.messages)
+    assert_equal(3, status.unseen)
+    assert_equal(2, status.recent)
+  end
+
+  def test_fetch
+    mails = @mailbox.fetch([1])
+    assert_equal(1, mails.length)
+    assert_equal(@uid1, mails[0].uid)
+    assert_equal(@mail1, mails[0].to_s)
+
+    mails = @mailbox.fetch([1, 4, 5])
+    assert_equal(3, mails.length)
+    assert_equal(@uid1, mails[0].uid)
+    assert_equal(@mail1, mails[0].to_s)
+    assert_equal(@uid4, mails[1].uid)
+    assert_equal(@mail4, mails[1].to_s)
+    assert_equal(@uid5, mails[2].uid)
+    assert_equal(@mail5, mails[2].to_s)
+
+    mails = @mailbox.fetch([1, 3..5])
+    assert_equal(4, mails.length)
+    assert_equal(@uid1, mails[0].uid)
+    assert_equal(@mail1, mails[0].to_s)
+    assert_equal(@uid3, mails[1].uid)
+    assert_equal(@mail3, mails[1].to_s)
+    assert_equal(@uid4, mails[2].uid)
+    assert_equal(@mail4, mails[2].to_s)
+    assert_equal(@uid5, mails[3].uid)
+    assert_equal(@mail5, mails[3].to_s)
   end
 end
 
