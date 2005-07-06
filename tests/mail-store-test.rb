037da0369c74061c7e60074dd9b24ca5d338b99a
Shugo Maeda <shugo@ruby-lang.org>
2005-07-06 15:29:19 +0000

    added new option --rebuld-index. (closes #61)

--- tests/mail-store-test.rb
+++ tests/mail-store-test.rb
@@ -30,6 +30,7 @@ class XimapdMailStoreTest < Test::Unit::TestCase
 
   def test_import_mail
     mail_store = Ximapd::MailStore.new(@config)
+    mail_store.create_mailbox("test")
     mail1 = <<EOF.gsub(/\n/, "\r\n")
 From foobar@ruby-lang.org  Sat Apr  2 00:07:54 2005
 Date: Wed, 30 Mar 2005 17:34:46 +0900
@@ -75,6 +76,96 @@ EOF
     m = mails[0]
     assert_equal(uid2, m.uid)
     assert_equal(mail2.sub(/\AFrom.*\r\n/, ""), m.to_s)
+
+    mail3 = <<EOF.gsub(/\n/, "\r\n")
+From foobar@ruby-lang.org  Sat Apr  2 00:07:54 2005
+Date: Wed, 30 Mar 2005 17:34:46 +0900
+Message-ID: <41ECC569.8000603@ruby-lang.org>
+From: shugo@ruby-lang.org (Shugo Maeda
+Subject: bye
+To: Foo <foo@ruby-lang.org>
+In-Reply-To: <41C448BF.7080605@ruby-lang.org>
+Content-Type: text/plain; charset=US-ASCII
+
+Goodbye world
+EOF
+    uid3 = mail_store.import_mail(mail3, "test")
+    assert_equal(uid2 + 1, uid3)
+    test_mailbox = mail_store.mailbox_db.transaction {
+      mail_store.get_mailbox("test")
+    }
+    mails = test_mailbox.uid_fetch([uid3])
+    assert_equal(1, mails.length)
+    m = mails[0]
+    assert_equal(uid3, m.uid)
+    assert_equal(mail3.sub(/\AFrom.*\r\n/, ""), m.to_s)
+  end
+
+  def test_rebuild_index
+    mail_store = Ximapd::MailStore.new(@config)
+    mail_store.create_mailbox("test")
+    mail1 = <<EOF.gsub(/\n/, "\r\n")
+From foobar@ruby-lang.org  Sat Apr  2 00:07:54 2005
+Date: Wed, 30 Mar 2005 17:34:46 +0900
+Message-ID: <41ECC569.8000603@ruby-lang.org>
+From: Shugo Maeda <shugo@ruby-lang.org>
+Subject: =?ISO-2022-JP?B?GyRCJDMkcyRLJEEkTxsoQg==?=
+To: Foo <foo@ruby-lang.org>,
+        bar@ruby-lang.org
+Cc: Baz <"baz.."@ruby-lang.org>
+In-Reply-To: <41C448BF.7080605@ruby-lang.org>
+Content-Type: text/plain; charset=US-ASCII
+
+Hello world
+EOF
+    uid1 = mail_store.import_mail(mail1)
+    mail2 = <<EOF.gsub(/\n/, "\r\n")
+From foobar@ruby-lang.org  Sat Apr  2 00:07:54 2005
+Date: Wed, 30 Mar 2005 17:34:46 +0900
+Message-ID: <41ECC569.8000603@ruby-lang.org>
+From: shugo@ruby-lang.org (Shugo Maeda
+Subject: Bye
+To: Foo <foo@ruby-lang.org>,
+        bar@ruby-lang.org
+Cc: Baz <"baz.."@ruby-lang.org>
+In-Reply-To: <41C448BF.7080605@ruby-lang.org>
+Content-Type: text/plain; charset=US-ASCII
+
+Goodbye world
+EOF
+    uid2 = mail_store.import_mail(mail2)
+    mail3 = <<EOF.gsub(/\n/, "\r\n")
+From foobar@ruby-lang.org  Sat Apr  2 00:07:54 2005
+Date: Wed, 30 Mar 2005 17:34:46 +0900
+Message-ID: <41ECC569.8000603@ruby-lang.org>
+From: shugo@ruby-lang.org (Shugo Maeda
+Subject: bye
+To: Foo <foo@ruby-lang.org>
+In-Reply-To: <41C448BF.7080605@ruby-lang.org>
+Content-Type: text/plain; charset=US-ASCII
+
+Goodbye world
+EOF
+    uid3 = mail_store.import_mail(mail3, "test")
+    mail_store.rebuild_index
+
+    inbox = mail_store.mailbox_db.transaction {
+      mail_store.get_mailbox("INBOX")
+    }
+    uids = inbox.uid_search("hello")
+    assert_equal([uid1], uids)
+    uids = inbox.uid_search("bye")
+    assert_equal([uid2], uids)
+    uids = inbox.uid_search("from : shugo")
+    assert_equal([uid1, uid2], uids)
+    test_mailbox = mail_store.mailbox_db.transaction {
+      mail_store.get_mailbox("test")
+    }
+    mails = test_mailbox.uid_fetch([uid3])
+    assert_equal(1, mails.length)
+    m = mails[0]
+    assert_equal(uid3, m.uid)
+    assert_equal(mail3.sub(/\AFrom.*\r\n/, ""), m.to_s)
   end
 end
 
