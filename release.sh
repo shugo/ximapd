7aa54e2e6f6a9e44e9b8c42e485e3859ec1b8fcf
Shugo Maeda <shugo@ruby-lang.org>
2005-08-07 07:41:08 +0000

    changed VERSION to "0.0.3". added release.sh.

---
+++ release.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -e
+
+version_line=`grep '^  VERSION =' bin/ximapd`
+version=`expr "$version_line" : '  VERSION = "\(.*\)"'`
+svn copy -m "tagged version $version" https://projects.netlab.jp/svn/ximapd/trunk https://projects.netlab.jp/svn/ximapd/tags/$version
+svn export https://projects.netlab.jp/svn/ximapd/tags/$version ximapd-$version
+tar zcvf ximapd-$version.tar.gz ximapd-$version
+gpg -ba ximapd-$version.tar.gz
+scp ximapd-$version.tar.gz{,.asc} projects.netlab.jp:/var/www/projects.netlab.jp/ximapd/releases/
