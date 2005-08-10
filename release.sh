0a63a7fbff240b04cc3d1bb42b7c75abc66b3bba
Shugo Maeda <shugo@ruby-lang.org>
2005-08-10 07:26:38 +0000

    changed trunk to branches/0.0.

--- release.sh
+++ release.sh
@@ -4,7 +4,7 @@ set -e
 
 version_line=`grep '^  VERSION =' bin/ximapd`
 version=`expr "$version_line" : '  VERSION = "\(.*\)"'`
-svn copy -m "tagged version $version" https://projects.netlab.jp/svn/ximapd/trunk https://projects.netlab.jp/svn/ximapd/tags/$version
+svn copy -m "tagged version $version" https://projects.netlab.jp/svn/ximapd/branches/0.0 https://projects.netlab.jp/svn/ximapd/tags/$version
 svn export https://projects.netlab.jp/svn/ximapd/tags/$version ximapd-$version
 tar zcvf ximapd-$version.tar.gz ximapd-$version
 gpg -ba ximapd-$version.tar.gz
